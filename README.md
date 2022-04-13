d7734f6a581414d3f996e0f22e25862e6fdec15b
jerryfrancis-97 <francis.jerome999@gmail.com>
2022-04-13 19:34:05 +0530

    initial : added code for colormaps  graphs logging; also added state-dependent eta

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# flexible_option_learning
+# Flexible Option Learning
+
+This repository contains code for the paper [Flexible Option Learning](https://arxiv.org/abs/2112.03097) presented as a Spotlight at NeurIPS 2021. The implementation is based on [gym-miniworld](https://github.com/maximecb/gym-miniworld), OpenAI's  [baselines](https://github.com/openai/baselines) and the Option-Critic's [tabular implementation](https://github.com/jeanharb/option_critic/tree/master/fourrooms).
+
+
+Contents:
+- [FourRooms Experiments](#tabular-experiments-four-rooms)
+- [Continuous Control Experiments](#continuous-control-mujoco)
+- [Visual Navigation Experiments](#maze-navigation-miniworld)
+- [Citation](#cite)
+
+
+
+
+
+## Tabular Experiments (Four-Rooms)
+
+#### Installation and Launch code
+
+```
+pip install gym==0.12.1
+cd diagnostic_experiments/
+python main_fixpol.py --multi_option # for experiments with fixed options
+python main.py --multi_option # for experiments with learned options
+```
+
+
+## Continuous Control (MuJoCo)
+
+#### Installation
+
+```
+virtualenv moc_cc --python=python3
+source moc_cc/bin/activate
+pip install tensorflow==1.12.0 
+cd continuous_control
+pip install -e . 
+pip install gym==0.9.3
+pip install mujoco-py==0.5.1
+```
+#### Launch
+
+```
+cd baselines/ppoc_int
+python run_mujoco.py --switch --nointfc --env AntWalls --eta 0.9 --mainlr 8e-5 --intlr 8e-5 --piolr 8e-5
+```
+
+
+## Maze Navigation (MiniWorld)
+
+#### Installation
+
+```
+virtualenv moc_vision --python=python3
+source moc_vision/bin/activate
+pip install tensorflow==1.13.1
+cd vision_miniworld
+pip install -e .
+pip install gym==0.15.4
+```
+
+#### Launch
+
+```
+cd baselines/
+# Run agent in first task
+python run.py --alg=ppo2_options --env=MiniWorld-WallGap-v0 --num_timesteps 2500000 --save_interval 1000  --num_env 8 --noptions 4 --eta 0.7
+
+# Load and run agent in transfer task
+python run.py --alg=ppo2_options --env=MiniWorld-WallGapTransfer-v0 --load_path path/to/model --num_timesteps 2500000 --save_interval 1000  --num_env 8 --noptions 4 --eta 0.7
+```
+
+
+## Cite
+
+If you find this work useful to you, please consider adding us to your references. 
+
+
+```
+@inproceedings{
+klissarov2021flexible,
+title={Flexible Option Learning},
+author={Martin Klissarov and Doina Precup},
+booktitle={Thirty-Fifth Conference on Neural Information Processing Systems},
+year={2021},
+url={https://openreview.net/forum?id=L5vbEVIePyb}
+}
+```
---
+++ diagnostic_experiments/main.py
@@ -0,0 +1,265 @@
+import gym
+import argparse
+import numpy as np
+from fourrooms import Fourrooms
+import random
+import time
+import os
+
+from scipy.stats import entropy
+from scipy.special import expit
+from scipy.special import logsumexp
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib 
+import seaborn as sns
+from utils import *
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--discount', help='Discount factor', type=float, default=0.99)
+    parser.add_argument('--lr_term', help="Termination gradient learning rate", type=float, default=0.8)
+    parser.add_argument('--lr_intra', help="Intra-option gradient learning rate", type=float, default=0.8)
+    parser.add_argument('--lr_critic', help="Learning rate", type=float, default=0.8)
+    parser.add_argument('--nepisodes', help="Number of episodes per run", type=int, default=1000)
+    parser.add_argument('--nruns', help="Number of runs", type=int, default=1)
+    parser.add_argument('--nsteps', help="Maximum number of steps per episode", type=int, default=1000)
+    parser.add_argument('--noptions', help='Number of options', type=int, default=4)
+    parser.add_argument('--option_temperature', help="Temperature parameter for softmax", type=float, default=1e-1)
+    parser.add_argument('--action_temperature', help="Temperature parameter for softmax", type=float, default=1e-2)
+    parser.add_argument('--seed', help="seed", type=int, default=1)
+
+
+    parser.add_argument('--multi_option', help="multi updates", default=False, action='store_true')
+    parser.add_argument('--eta', help="Multi-updates hypereparameter", type=float, default=0.3)
+    parser.add_argument('--linear_eta', help="Multi-updates state-dependent linear hyperparameter", default=False, action='store_true')
+    parser.add_argument('--linear_eta_distance', help='Distance concept for Linear eta [eucliedean, cityblock]', type=str, default='euclidean')
+    parser.add_argument('--new_randomness', help="new degree of randomness", type=float, default=0.45)
+
+    args = parser.parse_args()
+    # Multi-updates state-dependent linear hyperparameter - based on distance concept
+    if args.linear_eta:
+        eta_state_dependent = StateDependentETA(args.linear_eta_distance)
+        print("State dependent eta!")
+    elif args.eta:
+        eta=args.eta
+        print(f"Eta fixed at {eta}")
+    args.lr_term =args.lr_intra
+    total_steps=0
+    start=time.time()
+    possible_next_goals = [74,75,84,85]
+    history_steps = np.zeros((args.nruns, args.nepisodes))
+    observation_history = np.zeros((args.nruns, args.nepisodes, args.noptions, 13, 13)) # 13x13 room
+    state_eta_history = np.zeros((args.nruns, args.nepisodes, 13, 13))
+    for run in range(args.nruns):
+        env = gym.make('Fourrooms-v0')
+        env.set_goal(62)
+        env.set_seed(args.seed+run)
+
+
+        np.random.seed(args.seed+run)
+        random.seed(args.seed+run)
+
+        features = Tabular(env.observation_space.n)
+        nfeatures, nactions = len(features), env.action_space.n
+
+
+        # The intra-option policies are linear-softmax functions
+        option_policies = [SoftmaxPolicy(nfeatures, nactions, args.action_temperature) for _ in range(args.noptions)]
+
+        # The termination function are linear-sigmoid functions
+        option_terminations = [SigmoidTermination(nfeatures) for _ in range(args.noptions)]
+
+        # Policy over options
+        meta_policy = SoftmaxPolicy(nfeatures, args.noptions, args.option_temperature)
+
+        # Different choices are possible for the critic. Here we learn an
+        # option-value function and use the estimator for the values upon arrival
+        critic = IntraOptionQLearning(args.discount, args.lr_critic, option_terminations, meta_policy.weights, meta_policy,args.noptions) 
+
+        # Improvement of the termination functions based on gradients
+        termination_improvement= TerminationGradient(option_terminations, critic, args.lr_term,args.noptions)
+
+        # Intra-option gradient improvement with critic estimator
+        intraoption_improvement = IntraOptionGradient(option_policies, args.lr_intra, args.discount, critic,args.noptions)
+        
+
+
+        tot_steps=0.
+        for episode in range(args.nepisodes):
+            if episode > 0 and episode == int(args.nepisodes/2.): ############################# Change time #############################
+                goal=possible_next_goals[args.seed % len(possible_next_goals)]
+                env.set_goal(goal)
+                env.set_randomness(args.new_randomness)
+                print('************* New goal : ', env.goal)
+
+
+
+            
+            last_opt=None
+            phi = features(env.reset())
+            option = meta_policy.sample(phi)
+            action = option_policies[option].sample(phi)
+            critic.start(phi, option)
+
+
+
+            action_ratios_avg=[]
+            
+            for step in range(args.nsteps):
+                observation, reward, done, _ = env.step(action)
+                next_phi = features(observation)
+
+                if option_terminations[option].sample(next_phi): 
+                    next_option = meta_policy.sample(next_phi)
+                else:
+                    next_option=option
+
+                next_action = option_policies[next_option].sample(next_phi)
+
+
+
+                ###Action ratios
+                action_ratios=np.zeros((args.noptions))
+                for o in range(args.noptions):
+                    action_ratios[o] = option_policies[o].pmf(phi)[action]
+                action_ratios= action_ratios / action_ratios[option]
+                action_ratios_avg.append(action_ratios)
+
+
+                # Prob of current option
+                one_hot = np.zeros(args.noptions)
+                if last_opt is not None:
+                    bet = option_terminations[last_opt].pmf(phi)
+                    one_hot[last_opt] = 1.
+                else:
+                    bet = 1.0
+                prob_curr_opt = bet * meta_policy.pmf(phi) + (1-bet)*one_hot
+                one_hot_curr_opt= np.zeros(args.noptions)
+                one_hot_curr_opt[option] = 1.
+                
+                # print("State: ", phi, type(phi), env.tocell[phi.item()])
+                if args.linear_eta:
+                    current_state = env.tocell[phi.item()]
+                    current_i, current_j = current_state
+                    current_goal = env.tocell[env.goal]
+                    eta_given_phi = eta_state_dependent.eta_wrt_goal(current_state, current_goal)
+                    prob_curr_opt= eta_given_phi * prob_curr_opt + (1-eta_given_phi) * one_hot_curr_opt
+                    #tracking cell activity in room
+                    state_eta_history[run, episode, current_i, current_j] = eta_given_phi
+
+                elif args.eta:    
+                    sampled_eta = float(np.random.rand() < eta)
+                    prob_curr_opt= eta * prob_curr_opt + (1-eta) * one_hot_curr_opt
+                    current_state = env.tocell[phi.item()]
+                    current_i, current_j = current_state
+                    #tracking cell activity in room
+                    state_eta_history[run, episode, current_i, current_j] = eta
+                
+                else:
+                    pass
+
+
+            
+                # Critic updates
+                critic.update(next_phi, next_option, reward, done, one_hot_curr_opt)
+
+
+                # Intra-option policy update
+                critic_feedback = reward + args.discount * critic.value(next_phi, next_option)
+                critic_feedback -= critic.value(phi, option)
+                if args.multi_option:
+                    intraoption_improvement.update(phi, option, action, reward, done, next_phi, next_option, critic_feedback,
+                        action_ratios, prob_curr_opt  )   
+                else:
+                    intraoption_improvement.update(phi, option, action, reward, done, next_phi, next_option, critic_feedback,
+                        np.ones_like(action_ratios), one_hot_curr_opt  ) 
+
+                # Termination update
+                if not done:
+                    termination_improvement.update(next_phi, option, one_hot_curr_opt )
+
+                #tracking cell activity in room
+                i,j = env.tocell[phi.item()]
+                observation_history[run, episode, option, i, j] += 1
+
+
+                last_opt=option
+                phi=next_phi
+                option=next_option
+                action=next_action
+
+
+                if done:
+                    break
+
+
+            tot_steps+=step
+            history_steps[run, episode] = step
+            end=time.time()
+            print('Run {} Total steps {} episode {} steps {} FPS {:0.0f} '.format(run,tot_steps, episode, step,   int(tot_steps/ (end- start)) )  )
+
+    
+    if args.multi_option:
+        new_folder_name = f'multi_option_noptions{args.noptions}_nruns{args.nruns}_nepisodes{args.nepisodes}_nsteps{args.nsteps}'
+    else:
+        new_folder_name = f'single_option_noptions{args.noptions}_nruns{args.nruns}_nepisodes{args.nepisodes}_nsteps{args.nsteps}'
+    if args.linear_eta:
+        new_folder_name += f"_LINEAR_eta_distance_{args.linear_eta_distance}"
+    else:
+        new_folder_name += f"_fixed_eta_{args.eta}"
+
+    if os.path.exists(new_folder_name):
+        pass 
+    else:
+        os.mkdir(new_folder_name)
+        print("new folder created: ", new_folder_name)
+
+    #saving obsrvations and history
+    import pickle as pkl 
+    with open(os.path.join(new_folder_name, "history_steps.pkl"), "wb") as f:
+        pkl.dump(history_steps, f)
+    with open(os.path.join(new_folder_name,"observation_history.pkl"), "wb") as f:
+        pkl.dump(observation_history, f)
+    with open(os.path.join(new_folder_name,"state_eta_history.pkl"), "wb") as f:
+        pkl.dump(state_eta_history, f)
+
+    #plottng grahs
+    avg_steps_per_episode = exponential_smoothing(np.mean(history_steps, axis=0))
+    CI = 0.95
+    plt.plot(range(args.nepisodes), avg_steps_per_episode, color='blue', label='Avg steps in completion with ' + str(CI) + '% CI')
+    #confidence interval
+    ci = CI * np.std(avg_steps_per_episode)/np.mean(avg_steps_per_episode)
+    plt.fill_between(range(args.nepisodes), (avg_steps_per_episode-ci), (avg_steps_per_episode+ci), color='cyan', alpha=0.3)
+    plt.xlabel("Episodes")
+    plt.ylabel("Avg steps")
+    plt.savefig(os.path.join(new_folder_name, "Graph1.jpg"))
+    plt.close("all")
+    
+    activity_per_run = np.mean(observation_history, axis=0)
+    activity_per_episode = np.mean(activity_per_run, axis=0)
+    
+    #colormap for visited states wrt. options
+    for option in range(args.noptions):
+        print('Option no.: ', option)
+        full_env = activity_per_episode[option,:,:]
+        new_virdis = cm.get_cmap('viridis', 5)
+        plt.pcolormesh(full_env, cmap = new_virdis)
+        plt.colorbar()
+        plt.title(f"Colormap of freq. of visited states in Option {option}")
+        plt.savefig(os.path.join(new_folder_name, "Colormap_option"+str(option)+'.jpg'))
+        plt.close("all")
+
+    #colormap for state_related eta
+    eta_state_activity_per_run = np.mean(state_eta_history, axis=0)
+    eta_state_activity_per_episode = np.mean(eta_state_activity_per_run, axis=0)
+    eta_full_env = eta_state_activity_per_episode
+    new_inferno = cm.get_cmap('inferno', 5)
+    plt.pcolormesh(eta_full_env, cmap = new_inferno)
+    plt.colorbar()
+    plt.title('Colormap for eta in visited states')
+    plt.savefig(os.path.join(new_folder_name, "Colormap_linear_eta_state.jpg"))
+    plt.close("all")
+
+    print("Completed!")
